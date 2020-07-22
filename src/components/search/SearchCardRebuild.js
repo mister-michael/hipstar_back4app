9de35ecc065c11416bec79706f331c47f46b1fbe
Michael Clark <michaelclarknashville@protonmail.com>
2020-07-21 19:48:53 -0500

    handle love hate delete and update, page not refreshing after first change

--- src/components/search/SearchCardRebuild.js
+++ src/components/search/SearchCardRebuild.js
@@ -5,12 +5,19 @@ import dbAPI from "../../modules/dbAPI";
 const SearchCardRebuild = props => {
 
     const [buttonClass, setButtonClass] = useState({ loveClass: "closeButtonColor", hateClass: "closeButtonColor" })
-
     const [buttonText, setButtonText] = useState({ loveText: "Love", hateText: "Hate" })
+    const [hateButtonDisabled, setHateButtonDisabled] = useState(false)
+    const [LoveButtonDisabled, setLoveButtonDisabled] = useState(false)
+    const [deleteButtonDisabled, setDeleteButtonDisabled] = useState(true)
+    const [LHid, setLHid] = useState(null)
+
 
     const [movie, setMovie] = useState(props.result)
 
-    const [refresh, setRefresh] = useState(false)
+
+    const [refresh, setRefresh] = useState(null)
+
+    const activeUserId = sessionStorage.getItem("userId")
 
     console.log(props.result)
 
@@ -34,43 +41,95 @@ const SearchCardRebuild = props => {
                 const buttonTextState = { ...buttonText }
 
                 if (res.length > 0 && res[0].attributes.isHated === true) {
-                    console.log(res.length, "= RES LENGTH", res[0].attributes.isHated, "= isHated")
-                    console.log("if 1")
+                    // console.log(res.length, "= RES LENGTH", res[0].attributes.isHated, "= isHated")
+                    // console.log("if 1")
                     buttonClassState["hateClass"] = "profileHatedButton"
                     buttonTextState["hateText"] = "Hated"
                     setButtonClass(buttonClassState)
                     setButtonText(buttonTextState)
-                    
+                    setHateButtonDisabled(true)
+                    setDeleteButtonDisabled(false)
+                    setLHid(res[0].id)
+
                 } else if (res.length > 0 && res[0].attributes.isHated === false) {
-                    console.log(res.length, "= RES LENGTH", res[0].attributes.isHated, "= isHated")
-                    console.log("if 2");
+                    // console.log(res.length, "= RES LENGTH", res[0].attributes.isHated, "= isHated")
+                    // console.log("if 2");
                     buttonClassState["loveClass"] = "profileLovedButton"
                     buttonTextState["loveText"] = "Loved"
                     setButtonClass(buttonClassState)
                     setButtonText(buttonTextState)
+                    setLoveButtonDisabled(true)
+                    setDeleteButtonDisabled(false)
+                    setLHid(res[0].id)
                 } else {
                     setButtonClass(buttonClassState)
                     setButtonText(buttonTextState)
+                    setDeleteButtonDisabled(true)
+
                 }
             })
     };
 
-    const updateLoveHate = () => {
-        
+    async function handleLove() {
+        const loveHateObj = {
+            userId: activeUserId,
+            dbid: props.result.id,
+            isHated: false
+        };
+        if (LHid === null) {
+            await dbAPI.createNewObjectByClassName("loveHates", loveHateObj)
+                .then(res => console.log(res));
+            setRefresh(true)
+        } else {
+            await dbAPI.saveEditedObjectByClassNameAndObjId("loveHates", LHid, loveHateObj)
+                .then(res => console.log(res));
+        }
+    };
+
+    async function handleHate() {
+        const loveHateObj = {
+            userId: activeUserId,
+            dbid: props.result.id,
+            isHated: true
+        };
+        if (LHid === null) {
+            await dbAPI.createNewObjectByClassName("loveHates", loveHateObj)
+                .then(res => console.log(res));
+            setRefresh(true)
+        } else {
+            await dbAPI.saveEditedObjectByClassNameAndObjId("loveHates", LHid, loveHateObj)
+                .then(res => console.log(res));
+        }
+    };
+
+    async function handleDelete() {
+        await dbAPI.deleteObjectByClassNameAndId("loveHates", LHid)
+            .then(res => console.log(res));
+        setRefresh(true)
     }
+
 
     useEffect(() => {
         isMovieRated();
-    }, [movie])
+    }, [refresh])
 
     return (
         <>
             <div className="movieCard">
                 <CardImg id="" top src={imageHandler()} alt={`${props.result.title} poster`} className="cardImage" />
                 <div>{props.result.title}</div>
-                <button className={buttonClass.loveClass}>{buttonText.loveText}</button>
-                <button className={buttonClass.hateClass}>{buttonText.hateText}</button>
-                <button className="closeButtonColor">x</button>
+                <button
+                    className={buttonClass.loveClass}
+                    onClick={handleLove}
+                    disabled={LoveButtonDisabled}>{buttonText.loveText}</button>
+                <button
+                    className={buttonClass.hateClass}
+                    onClick={handleHate}
+                    disabled={hateButtonDisabled}>{buttonText.hateText}</button>
+                <button
+                    onClick={handleDelete}
+                    className="closeButtonColor"
+                    disabled={deleteButtonDisabled}>x</button>
             </div>
         </>
     )
